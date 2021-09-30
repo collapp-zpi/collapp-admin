c610c473389ced8304112e1a9b87006f4bcbb47c
Karol Sitarz <ka.sitarz@gmail.com>
2021-09-30 21:39:15 +0200

    add basic prisma configuration

---
+++ pages/api/developers.ts
@@ -0,0 +1,18 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { PrismaClient } from '@prisma/client'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+const prisma = new PrismaClient()
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== 'GET') {
+    return res.status(405).json({ message: 'Method not allowed' })
+  }
+
+  const developers = await prisma.developer.findMany()
+
+  res.json(developers)
+}

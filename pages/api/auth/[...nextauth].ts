42e36ba550c0cb8c4841804283d704a2ef6170a0
PepeKwapien <pppkwapien@gmail.com>
2021-10-10 17:19:22 +0200

    Trying to stop redirecting while signing in

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth";
 import EmailProvider from "next-auth/providers/email";
-import {PrismaExtendedAdapter} from "../../../config/PrismaExtendedAdapter";
+import { PrismaExtendedAdapter } from "../../../config/PrismaExtendedAdapter";
 
 export default NextAuth({
   providers: [
@@ -10,22 +10,22 @@ export default NextAuth({
         port: Number(process.env.EMAIL_SMTP_PORT),
         auth: {
           user: process.env.EMAIL_SERVER_USER,
-          pass: process.env.EMAIL_SERVER_PASSWORD
-        }
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
       },
-      from: process.env.EMAIL_FROM
+      from: process.env.EMAIL_FROM,
     }),
   ],
-  adapter: PrismaExtendedAdapter('admin'),
-  secret: 'secret',
+  adapter: PrismaExtendedAdapter("admin"),
+  secret: "secret",
   callbacks: {
     async signIn({ user: { email }, email: { verificationRequest } }) {
       // if user attempts to send a verification email
       if (verificationRequest) {
         // check if there is a user with that email
-        return true
+        return true;
       }
-      return true
-    }
-  }
-})
+      return true;
+    },
+  },
+});
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,43 @@
-import type {InferGetServerSidePropsType} from 'next'
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import {signOut, useSession} from "next-auth/react";
-import { signIn } from "next-auth/react"
+import type { InferGetServerSidePropsType } from "next";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import { signOut, useSession } from "next-auth/react";
+import { signIn } from "next-auth/react";
+import { useState } from "react";
 
 export async function getServerSideProps() {
-  const res = await fetch(`${process.env.BASE_URL}/api/developers`)
-  const developers = await res.json()
+  const res = await fetch(`${process.env.BASE_URL}/api/developers`);
+  const developers = await res.json();
 
   return {
-    props: { developers }
-  }
+    props: { developers },
+  };
 }
 
-const Home = (props: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-  const { data } = useSession()
+const Home = (
+  props: InferGetServerSidePropsType<typeof getServerSideProps>
+) => {
+  const { data } = useSession();
+  const [isError, setError] = useState(false);
+  const sendEmail = async () => {
+    //e.preventDefault();
+    const response = await signIn("email", {
+      redirect: false,
+      email: "24494222223330@stuuudent.pwr.edu.pl",
+    });
+    console.log(response);
+  };
 
   if (!data)
     return (
       <div className={styles.container}>
-        <button onClick={() => signIn()}>Sign in</button>
+        <form>
+          <label htmlFor="email">Email</label>
+          <input type="text" id="email" name="email" />
+          <button onSubmit={sendEmail}>Sign in</button>
+        </form>
       </div>
-    )
+    );
 
   return (
     <div className={styles.container}>
@@ -33,12 +49,10 @@ const Home = (props: InferGetServerSidePropsType<typeof getServerSideProps>) => 
       {console.log(props.developers)}
       <main className={styles.main}>
         <button onClick={() => signOut()}>Sign out</button>
-        <div>
-          {props.developers.length}
-        </div>
+        <div>{props.developers.length}</div>
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;

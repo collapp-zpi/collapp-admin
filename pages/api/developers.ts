2e81a5db48f9a984225369f5cb7b87485a325d11
PepeKwapien <pppkwapien@gmail.com>
2021-10-12 17:13:17 +0200

    Expoerted prisma client and added signin filter

--- pages/api/developers.ts
+++ pages/api/developers.ts
@@ -1,9 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { PrismaClient } from '@prisma/client'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { getSession } from 'next-auth/react'
-
-const prisma = new PrismaClient()
+import {prisma} from '../../config/PrismaClient'
 
 export default async function handler(
   req: NextApiRequest,

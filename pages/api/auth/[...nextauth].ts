fe926acb6529d3a1868a61bed2d403aebfef09be
Karol Sitarz <ka.sitarz@gmail.com>
2021-10-12 17:35:38 +0200

    minor eslint and prettier fixes

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,7 +1,7 @@
 import NextAuth from 'next-auth'
 import EmailProvider from 'next-auth/providers/email'
 import { PrismaExtendedAdapter } from '../../../config/PrismaExtendedAdapter'
-import {prisma} from '../../../config/PrismaClient'
+import { prisma } from '../../../config/PrismaClient'
 
 export default NextAuth({
   providers: [
@@ -18,19 +18,23 @@ export default NextAuth({
     }),
   ],
   pages: {
-    signIn: "../../",
+    signIn: '../../',
     error: '../../error',
-    signOut: "../../",
-    verifyRequest: "../../",
-    newUser: "../../"
+    signOut: '../../',
+    verifyRequest: '../../',
+    newUser: '../../',
   },
   adapter: PrismaExtendedAdapter('admin'),
   secret: 'secret',
   callbacks: {
-    async signIn({ user: { email }}) {
-      const admin = await prisma.adminUser.findUnique({where: {email: email!}})
+    async signIn({ user: { email } }) {
+      if (!email) return false
 
-      if(admin){
+      const admin = await prisma.adminUser.findUnique({
+        where: { email },
+      })
+
+      if (admin) {
         return true
       }
       return false
--- pages/api/developers.ts
+++ pages/api/developers.ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { getSession } from 'next-auth/react'
-import {prisma} from '../../config/PrismaClient'
+import { prisma } from '../../config/PrismaClient'
 
 export default async function handler(
   req: NextApiRequest,

0d8618c6bc1f3f9a41d2751a9f724f81b6cb5bbc
PepeKwapien <pppkwapien@gmail.com>
2021-10-21 22:23:02 +0200

    Plugins decorators

--- pages/api/plugins/[[...params]].ts
+++ pages/api/plugins/[[...params]].ts
@@ -1,1 +1,20 @@
 import { createHandler, Get, Param } from '@storyofams/next-api-decorators'
+import { prisma } from '../../../config/PrismaClient'
+import { NextAuthGuard } from '../../../shared/utils/apiDecorators'
+
+@NextAuthGuard()
+class Plugins {
+  @Get()
+  getPluginList() {
+    return prisma.draftPlugin.findMany()
+  }
+
+  @Get('/:id')
+  getPluginWithId(@Param('id') id: string) {
+    return prisma.draftPlugin.findFirst({
+      where: {
+        id: id as string,
+      },
+    })
+  }
+}

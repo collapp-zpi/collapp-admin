48ec260c90a0773ebc409297bf9d332a9ca761d7
PepeKwapien <pppkwapien@gmail.com>
2021-10-23 14:19:40 +0200

    Auth error adjusted to include ErrorPage component

--- pages/error.tsx
+++ pages/error.tsx
@@ -1,9 +1,17 @@
+import ErrorPage from 'components/ErrorPage'
+import { useRouter } from 'next/router'
 import React from 'react'
 
 export default function Error() {
-  return (
-    <>
-      <h1>There was an error</h1>
-    </>
-  )
+  const router = useRouter()
+  const error = router.query.error ? router.query.error : 'Forbidden'
+
+  let msg = ''
+  if (Array.isArray(error)) {
+    msg = error[0]
+  } else {
+    msg = error
+  }
+
+  return <ErrorPage statusCode={401} message={msg}></ErrorPage>
 }

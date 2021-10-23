1a9b31a66bc4190d0aeb51aa672827c16f711836
PepeKwapien <pppkwapien@gmail.com>
2021-10-23 14:44:36 +0200

    Added basic style to error page

--- components/ErrorPage.tsx
+++ components/ErrorPage.tsx
@@ -8,10 +8,13 @@ const ErrorPage = ({
   message: string
 }) => {
   return (
-    <>
-      <h1>{statusCode}</h1>
-      <h2>{message}</h2>
-    </>
+    <main className="flex h-full min-h-screen">
+      <div className="flex items-center m-auto bg-purple-50 shadow-xl py-6 px-10 align-middle">
+        <h1 className="mr-10 text-4xl">{statusCode}</h1>
+        <div className="bg-black w-px h-12" />
+        <h2 className="pl-2">{message}</h2>
+      </div>
+    </main>
   )
 }
 
